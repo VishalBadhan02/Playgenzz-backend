2595d3b46d2241c4505359862830be464e84e5d8
Vishal Badhan <135725438+VishalBadhan02@users.noreply.github.com>
2025-04-21 18:11:57 +0530

    sd

--- tournament-service/src/controllers/TournamentController.js
+++ tournament-service/src/controllers/TournamentController.js
@@ -397,5 +397,6 @@ const getUserRegisteredTournament = async (req, res) => {
 module.exports = {
     setEntry, setTeam, setFixtures, UpdateWinner, getSelectedRound, login, deleteTeam, getTournaments, updatePayment, handleRegister, fetchtournament,
     getUserRegisteredTournament
+
     
 }
