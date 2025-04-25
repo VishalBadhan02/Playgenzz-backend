a34036a2331f984f607b8b28e9746af5534f6b84
Vishal Badhan <135725438+VishalBadhan02@users.noreply.github.com>
2025-04-25 23:09:32 +0530

    sd

--- tournament-service/src/services/fixtureService.js
+++ tournament-service/src/services/fixtureService.js
@@ -80,7 +80,6 @@ const generateFixtures = async (input) => {
     // Here you would generate the fixtures and store them
     return { roundNumber, teams: result };
 };
-
 module.exports = {
     generateFixtures
 };
