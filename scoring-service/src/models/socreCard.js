cbe13c0fe0c320e5d383886263a8a412a011968c
Vishal Badhan <135725438+VishalBadhan02@users.noreply.github.com>
2025-05-14 11:50:12 +0530

    sd

--- scoring-service/src/models/socreCard.js
+++ scoring-service/src/models/socreCard.js
@@ -123,6 +123,7 @@ ScoreCardSchema.index({ 'teams.teamA.teamId': 1 });
 ScoreCardSchema.index({ 'teams.teamB.teamId': 1 });
 
 
+
 // ScoreCardSchema.methods.updatePlayerStats = async function (teamId, playerId, stats) {
 //     try {
 //         // Determine which team the player belongs to
