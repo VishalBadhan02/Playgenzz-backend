75b0439046c9bbe86c218556bca7f06f460ce1e6
Vishal Badhan <135725438+VishalBadhan02@users.noreply.github.com>
2025-05-15 12:16:14 +0530

    sd

--- scoring-service/src/controllers/ScoringController.js
+++ scoring-service/src/controllers/ScoringController.js
@@ -207,6 +207,7 @@ const calculateProjectedScore = (score) => {
 
 
 
+
 const handleScore = async (req, res) => {
     // try {
     //     const { matchId, tossWinner, tossWinnerName, sportSpecificDetails } = req.body;
