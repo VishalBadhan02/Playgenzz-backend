1555d6fefaed2601ad8be67e39fec4ad3134301a
Vishal Badhan <135725438+VishalBadhan02@users.noreply.github.com>
2025-05-29 14:04:25 +0530

    sd

--- auth-service/src/grpc-clients/venueClient.js
+++ auth-service/src/grpc-clients/venueClient.js
@@ -3,7 +3,7 @@ const protoLoader = require('@grpc/proto-loader');
 const path = require('path');
 
 // Define the path to the user.proto file
-const USER_PROTO_PATH = path.join(__dirname, '../protos/user.proto');
+const USER_PROTO_PATH = path.join(__dirname, '../../../protos/user.proto');
 
 // Load the user.proto file
 const packageDefinition = protoLoader.loadSync(USER_PROTO_PATH);
