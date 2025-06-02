2e3fd62a8bdbdf1c79bae362b26034588b839668
Vishal Badhan <135725438+VishalBadhan02@users.noreply.github.com>
2025-06-02 15:43:02 +0530

    sd

--- user-service/src/grpc-clients/scoreClient.js
+++ user-service/src/grpc-clients/scoreClient.js
@@ -1,6 +1,7 @@
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const path = require('path');
+const Config = require('../config');
 
 // Define the path to the user.proto file
 const SCORE_PROTO_PATH = path.join(__dirname, '../../../protos/user.proto');
@@ -11,7 +12,7 @@ const scoreProto = grpc.loadPackageDefinition(packageDefinition).user;
 
 // Create the gRPC client for the user service
 const scoreClient = new scoreProto.UserService(
-    'localhost:50051', // Address of the user service
+    `${Config.SCORING_GRPC_HOST}:${Config.SCORING_GRPC_PORT}`, // Address of the user service
     grpc.credentials.createInsecure()
 );
 
